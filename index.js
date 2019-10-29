67ff01bdfcfedbf1a87d06c8ba082d399a171dfc
github username <github email address>
2019-10-28 17:03:34 -0700

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ main.remove();
 
 
 let newHeader = document.createElement('h1');
-newHeader.setAttribute("id", "victory");
+newHeader.id = "victory";
 
 newHeader.innerHTML = "Daniel is the champion";
