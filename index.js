ac23f5bf49bd9c618a1db6de7884e05c36b8b11e
Andy Read <andyreadpnw@gmail.com>
2019-10-28 17:04:02 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+document.querySelector('main#main').remove();
+
+let newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory')
+
+newHeader.innerHTML = '<h1>victory is the champion</h1>';
+
+newHeader.innerText = 'YOUR-NAME is the champion';
